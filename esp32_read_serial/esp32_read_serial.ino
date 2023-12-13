ddd1f0b56a9b77f785bf460c028404b3b7f34c57
Zufaruu <zufarbagas28@gmail.com>
2023-12-13 10:56:50 +0700

    serial command transceive test

--- esp32_read_serial/esp32_read_serial.ino
+++ esp32_read_serial/esp32_read_serial.ino
@@ -1,46 +1,17 @@
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-
-// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+byte b;
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
-
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;);
-  }
-
-  delay(2000);
-  display.clearDisplay();
-  display.setTextSize(2);
-  display.setTextColor(WHITE);
+  Serial.setTimeout(1);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  if(Serial.available()){
-    while(Serial.available()){
-      display.clearDisplay();
-      display.setCursor(0, 25);
-      display.print(Serial.read());
-      display.display();
-      delay(500)
-    }
+void  loop() {
+  while (!Serial.available());
+  // x = Serial.readString().toInt();
+
+  while (Serial.available() > 0) {
+    byte b = Serial.read();
+    Serial.print(b);
   }
-  else{
-    display.clearDisplay();
-    display.setTextSize(1);
-    display.setTextColor(WHITE);
-    display.setCursor(0, 10);
-    // Display static text
-    display.println("Hello, world!");
-    display.display(); 
-    delay(500);
-  } 
+  
 }
--- serial_gimbal_test.py
+++ serial_gimbal_test.py
@@ -1,27 +1,22 @@
 import serial
+import time
 
-# testing windows + arduino
-ser = serial.Serial(
-    port='COM9',
-    baudrate=115200,
-)
+Serial = serial.Serial(port='COM9',  baudrate=115200, timeout=.1)
 
-# ser = serial.Serial(
-#     port='/dev/ttyUSB0',
-#     baudrate=19200,
-#     parity=serial.PARITY_NONE,
-#     stopbits=serial.STOPBITS_ONE,
-#     bytesize=serial.EIGHTBITS
-# )
+def write_read(x):
+    # arduino.write(bytes(x,  'utf-8'))
+    Serial.write(x)
+    time.sleep(0.05)
+    data = Serial.readline()
+    return  data
 
-print(ser.isOpen())
-thestring = "FF 01 0F 10 00 01 01 00 00 00 00 0A 00 00 00 0A 00 00 00 16"
-thestring = bytes.fromhex(thestring)
-print(thestring)
+if __name__ == "__main__":
+    while True:
+        num = input("Enter a number: ")
+        thestring = "FF 01 0F 10 00 01 01 00 00 00 00 0A 00 00 00 0A 00 00 00 16"
+        thestring = bytes.fromhex(thestring)
+        value  = write_read(thestring)
+        print(value)
 
-# ser.write(thestring)
-ser.write(thestring)
-
-ser.close()
 
 
