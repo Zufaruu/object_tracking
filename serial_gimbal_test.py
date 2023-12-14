7b8db6531cb288c55398fd4fcd88ab794ca645ca
Zufaruu <zufarbagas28@gmail.com>
2023-12-14 10:53:53 +0700

    fixing chekcsum format

--- serial_gimbal_test.py
+++ serial_gimbal_test.py
@@ -19,27 +19,23 @@ def control_parser():
     Header = "FF 01 0F 10"
     RM = " 00" # Roll mode | 00 No control, 01 Mode Speed, 02 Mode Speed Angle
     PM = " 01" # Pitch Mode | 00 No control, 01 Mode Speed, 02 Mode Speed Angle
-    YM = " 00" # Yaw Mode | 00 No control, 01 Mode Speed, 02 Mode Speed Angle
+    YM = " 01" # Yaw Mode | 00 No control, 01 Mode Speed, 02 Mode Speed Angle
     Rsl = " 00" # Roll speed low byte 
     Rsh = " 00" # Roll speed high byte | 0.1220740379 degree/sec | (2 byte signed, little-endian order)
     Ral = " 00" # Roll angle low byte 
     Rah = " 00" # Roll angle  high byte | 0.02197265625 degree | (2 byte signed, little-endian order)
-    Psl = " EB" # Pitch speed low byte 
-    Psh = " FF" # Pitch speed high byte | 0.1220740379 degree/sec | (2 byte signed, little-endian order)
+    Psl = " 00" # Pitch speed low byte 
+    Psh = " 00" # Pitch speed high byte | 0.1220740379 degree/sec | (2 byte signed, little-endian order)
     Pal = " 00" # Pitch angle low byte 
     Pah = " 00" # Pitch angle  high byte | 0.02197265625 degree | (2 byte signed, little-endian order)
-    Ysl = " 00" # Yaw speed low byte 
-    Ysh = " 00" # Yaw speed high byte | 0.1220740379 degree/sec | (2 byte signed, little-endian order)
+    Ysl = " E1" # Yaw speed low byte 
+    Ysh = " FF" # Yaw speed high byte | 0.1220740379 degree/sec | (2 byte signed, little-endian order)
     Yal = " 00" # Yaw angle low byte 
     Yah = " 00" # Yaw angle  high byte | 0.02197265625 degree | (2 byte signed, little-endian order)
-    CS =  "{0:x}".format(((int(RM[-2:], 16) + int(PM[-2:], 16) + int(YM[-2:], 16) 
+    CS =  "{:02x}".format(((int(RM[-2:], 16) + int(PM[-2:], 16) + int(YM[-2:], 16) 
                            + int(Rsl[-2:], 16) + int(Rsh[-2:], 16) + int(Ral[-2:], 16) + int(Rah[-2:], 16) 
                            + int(Psl[-2:], 16) + int(Psh[-2:], 16) + int(Pal[-2:], 16) + int(Pah[-2:], 16) 
                            + int(Ysl[-2:], 16) + int(Ysh[-2:], 16) + int(Yal[-2:], 16) + int(Yah[-2:], 16)) % 256))  # checksum
-    if len(CS) == 1:
-        CS = " 0" + CS
-    else:
-        CS = " " + CS
 
     command = Header + RM + PM + YM + Rsl + Rsh + Ral + Rah + Psl + Psh + Pal + Pah + Ysl + Ysh + Yal + Yah + CS
     
